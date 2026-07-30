a78e521fc5f4de2a0fc2d9fcabf514a69d6e79a3
mustafa64-stack <mustafa.93717@gmail.com>
2026-07-30 12:18:04 +0530

    Add Challenge 2: std::string upgrade

---
+++ quest2STR.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string first_name;
+    string last_name;
+    cout<<"Enter your first name :";
+    cin>>first_name;
+    cout<<"Enter your last name :";
+    cin>>last_name;
+    cout<<"Full name :"<<first_name+" "+last_name<<endl;
+    return 0;
+}
