965a75e016618a98be54e87c642cfd85f0a9447a
mustafa64-stack <mustafa.93717@gmail.com>
2026-07-30 12:08:45 +0530

    Completed Challenge 1: Modern IO

---
+++ quest1IO.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter any number :";
+    int n;
+    cin>>n;
+    cout<<"The number multiplied by 10 equals :"<<n*10<<endl;
+    return 0;
+}
